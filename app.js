b83b30d3afead666180e894189c2ad328c9a2cc8
Santimendezl <santimlacosta@gmail.com>
2023-03-27 19:40:56 +0000

    Domain Hacks

--- app.js
+++ app.js
@@ -1,14 +1,33 @@
-const pronoun = ["the", "our"];
-const adj = ["great", "big"];
-const noun = ["jogger", "racoon"];
-const ext = [".com", ".net", ".es", ".org"];
+// const pronoun = ["the", "our"];
+// const adj = ["great", "big"];
+// const noun = ["jogger", "racoon"];
+// const ext = ["com", "net", "es", "org"];
+
+// for (let i = 0; i < pronoun.length; i++) {
+//     for (let j = 0; j < adj.length; j++) {
+//         for (let k = 0; k < noun.length; k++) {
+//             for (let l = 0; l < ext.length; l++) {
+//                 console.log(pronoun[i] + adj[j] + noun[k] + "." + ext[l]);
+
+//             }
+//         }
+//     }
+// }
+const pronoun = ["the", "our", "my", "our"];
+const adj = ["great", "big", "special", "perfect"];
+const noun = ["stones", "cornet", "cyborg", "house"];
+const ext = ["com", "net", "es", "org"];
 
 for (let i = 0; i < pronoun.length; i++) {
     for (let j = 0; j < adj.length; j++) {
         for (let k = 0; k < noun.length; k++) {
-            for (let n = 0; n < ext.length; n++) {
-                console.log(pronoun[i] + adj[j] + noun[k] + ext[n]);
+            for (let l = 0; l < ext.length; l++) {
+                if (noun[k].endsWith(ext[l])) {
+                    let newNoun = noun[k].slice(0, noun[k].length - ext[l].length) + "." + ext[l];
+                    console.log(pronoun[i] + adj[j] + newNoun);
+                }
 
+                console.log(pronoun[i] + adj[j] + noun[k] + "." + ext[l]);
             }
         }
     }
